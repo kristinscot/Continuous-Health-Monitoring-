711c46945a91af5ffe28fc283e5f5ff704da6c87
Ciaran-McDJ <ciaranmcdj@gmail.com>
2026-01-18 23:40:55 -0500

    Added to Ciaran_Test for PPG code. Merged LED pulses and ADC measurements. Have framework for code

--- PPG_code/Ciaran_Testing/src/main.c
+++ PPG_code/Ciaran_Testing/src/main.c
@@ -1,11 +1,33 @@
 // Copied from 'digital_out' application
-
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 
+// Copied from 'analog_read_serial_out' application
+#include <zephyr/drivers/adc.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/util.h>
+
 //Required for high drive
 #include <zephyr/dt-bindings/gpio/nordic-nrf-gpio.h>
+
+
+
+// Initializing ADC stuff for analog input pin
+#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
+    !DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
+#error "No suitable devicetree overlay specified"
+#endif
+
+#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
+    ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
+
+static const struct adc_dt_spec adc_channels[] = {
+    DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
+                         DT_SPEC_AND_COMMA)
+};
+
 
 
 // Initializing LED GPIO pin stuff
@@ -23,6 +45,22 @@ static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios); //Gr
 static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios); //Red
 static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios); //Infrared
 
+
+
+// Initializing Variables and Parameters that will be used for sampling
+// NOTE: All 4 element arrays specify one value for each 'state'.
+// Each sampling period goes over all 4 states of 0-GreenOn, 1-RedOn, 2-InfraredON, 3-AllOff
+
+static const int stateDuration[4]  = {1000*1000, 1000*1000, 1000*1000, 1000*1000}; //us
+static const int settlingDuration[4] = {1000*100, 1000*100, 1000*100, 1000*100}; //us
+
+
+
+
+
+//SOME BASIC FUNCTIONS FOR SETTING THINGS UP
+
+/** Initialize one LED. returns 0 if succeeded*/
 static int init_led(const struct gpio_dt_spec *led)
 {
     if (!device_is_ready(led->port)) {
@@ -30,6 +68,23 @@ static int init_led(const struct gpio_dt_spec *led)
         return -1;
     }
     return gpio_pin_configure_dt(led, GPIO_OUTPUT_INACTIVE | NRF_GPIO_DRIVE_H0H1);
+}
+
+/** Initialize the ADC. returns 0 if succeeded*/
+static int init_adc() {
+    for (size_t i = 0; i < ARRAY_SIZE(adc_channels); i++) {
+        if (!adc_is_ready_dt(&adc_channels[i])) {
+            printf("ADC %s not ready\n", adc_channels[i].dev->name);
+            return -1;
+        }
+
+        int err = adc_channel_setup_dt(&adc_channels[i]);
+        if (err) {
+            printf("Channel setup failed (%d)\n", err);
+            return -1;
+        }
+    }
+    return 0;
 }
 
 static void turn_state_led_on(int curState)
@@ -74,18 +129,20 @@ static void turn_prev_state_led_off(int curState)
     }
 }
 
-// Initializing Variables and Parameters that will be used for sampling
-// NOTE: All 4 element arrays specify one value for each 'state'.
-// Each sampling period goes over all 4 states of 0-GreenOn, 1-RedOn, 2-InfraredON, 3-AllOff
-
-static const int stateDuration[4]  = {1000*1000, 1000*1000, 1000*1000, 1000*1000}; //us
-static const int settlingDuration[4] = {1000*100, 1000*100, 1000*100, 1000*100}; //us
 
 
 
 
 int main(void)
 {
+    int16_t sample;   // SIGNED buffer for reading adc values
+    struct adc_sequence sequence = {
+        .buffer = &sample,
+        .buffer_size = sizeof(sample),
+    };
+
+
+
     k_msleep(3000); // wait a minute for stuff to start. Otherwise I miss this print
     printf("Compiled %s at %s %s \n", __FILE__, __DATE__, __TIME__); //This is when *compiled* (helpful for knowing if you successfully uploaded newly compiled code)
 
@@ -96,12 +153,18 @@ int main(void)
         return -1;
     }
 
+    if (init_adc() != 0) {
+        printf("MYERROR: Failed to initialize ADC. Quitting main loop\n");
+        k_msleep(1000*10); //Don't quit for 10s, so I can read serial error message
+        return -1;
+    }
+
     while (1) {
         for (int state=0; state<4; state++) {
             // Goes over every state. 0-GreenOn, 1-RedOn, 2-InfraredON, 3-AllOff
 
             // Make sure LEDs are in correct state
-            printf("State %d\n", state);
+            printf("\nState %d\n", state);
             turn_prev_state_led_off(state);
             turn_state_led_on(state);
 
@@ -112,7 +175,33 @@ int main(void)
 
 
             // Take Measurements
-            k_usleep(stateDuration[state]);
+            // (TODO - add timing and store timestamp. And add taking multiple samples and add buffer to store multiple samples)
+            
+            // TODO - I can move this into a function and/or make it simpler by removing the for loop since only one channel
+            for (size_t i = 0; i < ARRAY_SIZE(adc_channels); i++) {
+                int32_t mv;
+
+                adc_sequence_init_dt(&adc_channels[i], &sequence);
+                int err = adc_read_dt(&adc_channels[i], &sequence);
+                if (err) {
+                    printf("adc_read failed (%d)\n", err);
+                    continue;
+                }
+
+                /* sample is already signed */
+                mv = sample;
+
+                printf("adc=%d \n", sample);
+
+
+                if (adc_raw_to_millivolts_dt(&adc_channels[i], &mv) == 0) {
+                    printf("mv=%d\n", mv);
+                } else {
+                    printf("(mV unavailable)\n");
+                }
+            }
+
+            k_usleep(stateDuration[state]); //Temporary for testing
 
 
 
